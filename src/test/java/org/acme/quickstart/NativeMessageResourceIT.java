119dca1e8ddea0424ef10006c0aed9e3e4648b0a
Maxim Burgerhout <maxim@redhat.com>
2019-09-06 12:00:22 +0200

    First simple start

---
+++ src/test/java/org/acme/quickstart/NativeMessageResourceIT.java
@@ -0,0 +1,9 @@
+package org.acme.quickstart;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeMessageResourceIT extends MessageResourceTest {
+
+    // Execute the same tests but in native mode.
+}
